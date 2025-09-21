5a84d84e8a966c8f8fb7a058c9cabba8ab3146ec
Ivan <icjfuncion@gmail.com>
2025-09-21 23:05:42 +0800

    03: Create and Read Employee feature

---
+++ test/controllers/employee_controller_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class EmployeeControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  # test "should get index" do
+  #   get employees_url
+  #   assert_response :success
+  #   assert_not_nil assigns(:employee)
+  #   assert_not_nil assigns(:employee_types)
+  #   assert_not_nil assigns(:employees)
+  # end
+end
