db8f550158ebb4d960fd8d78faccf4f8a583daa3
Ivan <icjfuncion@gmail.com>
2025-10-05 21:21:26 +0800

    Need to revise MVP

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  # def get_user_information_of_currently_logged_in_user
+  #   User.find_by(id: Current.user&.id)
+  #   Employee.find_by(id: Current.user&.employee_id)
+  # end
 end
