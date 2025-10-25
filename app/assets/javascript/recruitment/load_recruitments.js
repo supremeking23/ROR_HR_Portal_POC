48929dd7d80d0e3c2ec4ebcfc8e14b61b9f01999
Ivan <icjfuncion@gmail.com>
2025-10-25 19:55:42 +0800

    09: Add pagination on recruitment table using pagy gem

--- app/assets/javascript/recruitment/load_recruitments.js
+++ app/assets/javascript/recruitment/load_recruitments.js
@@ -1,11 +1,12 @@
 $(function() {
     loadRecruitments();
+    // fetchRecruitments();
 });
 
-function loadRecruitments() {
+function loadRecruitments(page = 1) {
     const recruitment_table = $('#recruitment_table tbody');
     $.ajax({
-        url: '/recruitments/load_recruitments',
+        url: `/recruitments/load_recruitments?page=${page}`,
         method: 'GET',
         dataType: 'json',
         success: function(data) {
@@ -30,9 +31,59 @@ function loadRecruitments() {
             });
             recruitment_table.append(rows);
             // You can add code here to update the UI with the loaded recruitments
+            renderPagination(data.pagy);
         },
         error: function(xhr, status, error) {
             console.error('Error loading recruitments:', error);
         }
     });
+}
+
+// function fetchRecruitments(page = 1) {
+//   fetch(`/recruitments/load_recruitments?page=${page}`)
+//     .then(res => res.json())
+//     .then(data => {
+//     //   renderRecruitments(data.recruitments);
+//       renderPagination(data.pagy);
+//     });
+// }
+
+function renderPagination(pagy) {
+  const container = document.getElementById("pagination-nav");
+  container.innerHTML = "";
+
+  const current = pagy.page;
+  const total = pagy.pages;
+  const range = 2; // how many pages to show around current
+  const pages = [];
+
+  /* TODO: Refactor so it may be readable */
+  for (let i = 1; i <= total; i++) {
+    if (
+      i === 1 || i === total || // always show first and last
+      Math.abs(i - current) <= range
+    ) {
+      pages.push(i);
+    } else if (pages[pages.length - 1] !== "...") {
+      pages.push("...");
+    }
+  }
+
+  container.innerHTML += `<li class="page-item"><button data-page="${pagy.prev}" class="page-link ${pagy.prev ? '': 'disabled'}">« Prev</button></li>`;
+
+  pages.forEach(p => {
+    if (p === "...") {
+      container.innerHTML += `<span>...</span>`;
+    } else {
+      container.innerHTML += `<li class="page-item"><button data-page="${p}" class="page-link ${p === current ? 'active' : ''}" >${p}</button></li>`;
+    }
+  });
+
+  container.innerHTML += `<li class="page-item"><button data-page="${pagy.next}" class="page-link  ${pagy.next ? '': 'disabled'}">Next »</button></li>`;
+
+  container.querySelectorAll("button[data-page]").forEach(btn => {
+    btn.addEventListener("click", () => {
+      loadRecruitments(btn.dataset.page);
+    });
+  });
 }
